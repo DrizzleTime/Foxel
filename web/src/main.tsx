ad016baaf94d553d09d7ebe3dca963a69dcb8431
shiyu <im@shiyu.dev>
2025-12-25 17:38:27 +0800

    chore(deps): update dependencies and remove unused package

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import '@ant-design/v5-patch-for-react-19';
 import 'antd/dist/reset.css';
 import './global.css';
 import { BrowserRouter } from 'react-router';
